8de9fe5538e5243e8fdcadef9a05c3986744ca35
DavidMarioLC <davidmariolc.dev@gmail.com>
2022-03-02 23:12:31 -0500

    :lipstick: [ADD] landingpage and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mybrown: "#16161A",
+        mypinksoft: "#F9E5DA",
+        mychocolate: "#391400",
+        myorange: "#EF6D58",
+        mysoftbrown: "rgba(57, 20, 0, 0.64)",
+      },
+    },
   },
   plugins: [],
 };
